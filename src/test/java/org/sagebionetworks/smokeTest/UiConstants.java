51ecc02f974e8a16577a363994aeba598c91fd86
Xavier Schildwachter <x.schildwachter@sagebase.org>
2012-08-07 11:25:04 -0700

    Adding ids for elements to check.

--- src/test/java/org/sagebionetworks/smokeTest/UiConstants.java
+++ src/test/java/org/sagebionetworks/smokeTest/UiConstants.java
@@ -5,17 +5,24 @@ public class UiConstants {
 	/*
 	 * Element ids for Selenium
 	 */
+	// Home page
 	public static final String ID_BTN_LOGIN = "id_btn_login";
 	public static final String ID_BTN_REGISTER = "id_btn_register";
+	
+	// User Home page
 	public static final String ID_BTN_USER = "id_btn_user";
-	public static final String ID_BTN_FORGOT_PWD = "id_btn_forgot_pwd";
-	public static final String ID_BTN_REGISTER2 = "id_btn_register2";
-	public static final String ID_BTN_LOGIN_AGAIN = "id_btn_login_again";
+	public static final String ID_MNU_USER_PROFILE = "id_mnu_user_profile";
+	public static final String ID_MNU_USER_LOGOUT = "id_mnu_user_logout";
+	
+	// Login page
 	public static final String ID_INP_EMAIL_NAME = "id_inp_email_name";
 	public static final String ID_INP_EMAIL_PASSWORD = "id_inp_email_password";
 	public static final String ID_BTN_LOGIN2 = "id_btn_login2";
-	public static final String ID_MNU_USER_PROFILE = "id_mnu_user_profile";
-	public static final String ID_MNU_USER_LOGOUT = "id_mnu_user_logout";
+	public static final String ID_BTN_FORGOT_PWD = "id_btn_forgot_pwd";
+	public static final String ID_BTN_REGISTER2 = "id_btn_register2";
+	
+	// Logout page
+	public static final String ID_BTN_LOGIN_AGAIN = "id_btn_login_again";
 	
 	/*
 	 * 
